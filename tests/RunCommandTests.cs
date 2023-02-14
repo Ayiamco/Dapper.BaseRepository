1a142c92ac0c078777198720b446e5db22e2cc8d
joseph achonu <josephachonu@gmail.com>
2023-02-14 12:42:44 +0100

    added test

--- tests/RunCommandTests.cs
+++ tests/RunCommandTests.cs
@@ -1,3 +1,4 @@
+using Dapper.BaseRepository.Components;
 using Dapper.BaseRepository.Config;
 using Moq;
 
@@ -6,16 +7,27 @@ namespace DapperBaseRepo.Tests
     public class RunCommandTests : BaseTestComponent
     {
         [Fact]
-        public async Task ShouldExecuteOnSqlServer_WhenCalledWithoutSpecifyingDbType()
+        public async Task ShouldExecuteOnSqlServer_WhenNoDbTypeIsSpecified()
         {
-            //Arrange
-
             //Act
             var resp = await baseRepo.RunCommand(dummySqlString, new { });
 
             //Assert
             dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlString, ConnectionStrings.SqlServerConnection, new { }), Times.Once);
-            loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldExecuteOnSqlServer_WhenCalledWithoutSpecifyingDbType)}"), Times.Once);
+            loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldExecuteOnSqlServer_WhenNoDbTypeIsSpecified)}"), Times.Once);
+            Assert.Equal(CommandResp.Success, resp);
+        }
+
+        [Fact]
+        public async Task ShouldCallSybase_WhenDbTypeIsSybase()
+        {
+            //Act
+            var resp = await baseRepo.RunCommand(dummySqlString, new { }, DbType.Sybase);
+
+            //Assert
+            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlString, ConnectionStrings.SybaseConnection, new { }), Times.Once);
+            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlString, ConnectionStrings.SqlServerConnection, new { }), Times.Never);
+            loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldCallSybase_WhenDbTypeIsSybase)}"), Times.Once);
             Assert.Equal(CommandResp.Success, resp);
         }
     }
