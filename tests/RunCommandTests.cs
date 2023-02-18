f7f2fbf54aeb6b23823f830f3f1328f187a6ac38
joseph achonu <josephachonu@gmail.com>
2023-02-18 13:56:49 +0100

    more test cases

--- tests/RunCommandTests.cs
+++ tests/RunCommandTests.cs
@@ -26,8 +26,23 @@ namespace DapperBaseRepo.Tests
 
             //Assert
             dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SybaseConnection, new { }), Times.Once);
-            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Never);
+            dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Never);
             loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldCallSybase_WhenDbTypeIsSybase)}"), Times.Once);
+            Assert.Equal(CommandResp.Success, resp);
+        }
+
+        [Fact]
+        public async Task ShouldCallSqlServer_WhenDbTypeIsSqlServer()
+        {
+            //Act
+            var resp = await baseRepo.RunCommand(dummySqlCommand, new { }, DbType.SqlServer);
+
+            //Assert
+            dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Once);
+            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SybaseConnection, new { }), Times.Never);
+            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Never);
+            dbExecutorMock.Verify(x => x.ExecuteOracleCommand(dummySqlCommand, ConnectionStrings.OracleConnection, new { }), Times.Never);
+            loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldCallSqlServer_WhenDbTypeIsSqlServer)}"), Times.Once);
             Assert.Equal(CommandResp.Success, resp);
         }
 
@@ -49,26 +64,26 @@ namespace DapperBaseRepo.Tests
         public async Task ShouldNotUseDefaultConnectionString_WhenConnectionIsProvided()
         {
             //Arrange
-            var connString1 = "newConnectionString";
-            var connString2 = "newConnectionString";
-            var connString3 = "newConnectionString";
+            var sybaseConnString = "sybaseConnString";
+            var sqlServerConnString = "sqlServerConnString";
+            var oracleConnString = "oracleConnString";
 
             //Act
-            var resp1 = await baseRepo.RunCommand(dummySqlCommand, new { }, connString1, DbType.Sybase);
-            var resp2 = await baseRepo.RunCommand(dummySqlCommand, new { }, connString2, DbType.SqlServer);
-            var resp3 = await baseRepo.RunCommand(dummySqlCommand, new { }, connString3, DbType.Oracle);
+            var sybaseResp = await baseRepo.RunCommand(dummySqlCommand, new { }, sybaseConnString, DbType.Sybase);
+            var sqlServerResp = await baseRepo.RunCommand(dummySqlCommand, new { }, sqlServerConnString, DbType.SqlServer);
+            var oracleServerResp = await baseRepo.RunCommand(dummySqlCommand, new { }, oracleConnString, DbType.Oracle);
 
             //Assert
             dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SybaseConnection, new { }), Times.Never);
             dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Never);
             dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, ConnectionStrings.OracleConnection, new { }), Times.Never);
-            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, connString1, new { }), Times.Once);
-            dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlCommand, connString2, new { }), Times.Once);
-            dbExecutorMock.Verify(x => x.ExecuteOracleCommand(dummySqlCommand, connString3, new { }), Times.Once);
+            dbExecutorMock.Verify(x => x.ExecuteSybaseCommand(dummySqlCommand, sybaseConnString, new { }), Times.Once);
+            dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlCommand, sqlServerConnString, new { }), Times.Once);
+            dbExecutorMock.Verify(x => x.ExecuteOracleCommand(dummySqlCommand, oracleConnString, new { }), Times.Once);
             loggerMock.Verify(x => x.LogInformation($"Successfully ran command from function: {nameof(ShouldNotUseDefaultConnectionString_WhenConnectionIsProvided)}"));
-            Assert.Equal(CommandResp.Success, resp1);
-            Assert.Equal(CommandResp.Success, resp2);
-            Assert.Equal(CommandResp.Success, resp3);
+            Assert.Equal(CommandResp.Success, sybaseResp);
+            Assert.Equal(CommandResp.Success, sqlServerResp);
+            Assert.Equal(CommandResp.Success, oracleServerResp);
         }
 
         [Fact]
@@ -86,6 +101,20 @@ namespace DapperBaseRepo.Tests
             loggerMock.Verify(x => x.LogError(It.IsAny<string>()));
         }
 
-        //should return failure
+        [Fact]
+        public async Task ShouldReturnFailure_WhenErrorsOccurs()
+        {
+            //Arrange
+            dbExecutorMock.Setup(x => x.ExecuteCommand(It.IsAny<string>(), ConnectionStrings.SqlServerConnection, new { })).
+                Throws(new Exception("Exception happened"));
+
+            //Act
+            var resp = await baseRepo.RunCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { });
+
+            //Assert
+            dbExecutorMock.Verify(x => x.ExecuteCommand(dummySqlCommand, ConnectionStrings.SqlServerConnection, new { }), Times.Once);
+            loggerMock.Verify(x => x.LogError(It.IsAny<string>()));
+            Assert.Equal(CommandResp.Failure, resp);
+        }
     }
 }
